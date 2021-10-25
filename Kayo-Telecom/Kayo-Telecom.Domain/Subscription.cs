b082faa2159f64571b9555f56012aa98379c8062
morostep <mstephan@skillstorm.com>
2021-10-24 22:15:25 -0500

    Updated

---
+++ Kayo-Telecom/Kayo-Telecom.Domain/Subscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kayo_Telecom.Domain
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+        public Plan Plan { get; set; }
+        public User User { get; set; }
+        public List<Device> Devices { get; set; }
+    }
+}
